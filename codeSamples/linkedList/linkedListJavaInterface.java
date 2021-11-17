a50cb88b9ae23016ff5574e780b37484ed629a45
Grace <gqxxnn@gmail.com>
2021-11-17 22:35:28 +0000

    linkedlist

---
+++ codeSamples/linkedList/linkedListJavaInterface.java
@@ -0,0 +1,10 @@
+
+public class linkedListJavaInterface {
+        public static void main(String[] arg) {
+
+                LinkedList<String> URLS = new LinkedList<String>();
+                URLS.add("Current");
+                URLS.addFirst("Back");
+                URLS.add("Previous");
+        }
+}
