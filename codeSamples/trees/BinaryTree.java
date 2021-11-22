7aa35b3d93a8ddf5d9c87a8eb0935aba984f8096
Grace <gqxxnn@gmail.com>
2021-11-22 22:58:22 +0000

    trees

---
+++ codeSamples/trees/BinaryTree.java
@@ -0,0 +1,13 @@
+public class BinaryTree {
+    Node root, parent;
+    
+    public class Node {
+        public int key;
+        public Node leftChild, rightChild;
+    
+    
+        public Node(int key){
+            this.key = key;
+        }
+    }
+}
